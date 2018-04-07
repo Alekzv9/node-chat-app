214a5e68e1d868ff02a84a061076f1dec26a33cf
alekzgrinder@gmail.com <alekzgrinder@gmail.com>
2018-04-06 20:29:35 -0500

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -38,11 +38,14 @@ socket.on('newLocationMessage', function (message) {
 
 $('#message-form').on('submit', function (e) {
     e.preventDefault();
+
+    var messageTextBox = $('#message');
+
     socket.emit('createMessage', {
         from: 'User',
-        text: $('#message').val()
+        text: messageTextBox.val()
     }, function () {
-
+        messageTextBox.val('');
     });
 });
 
@@ -52,12 +55,16 @@ locationButton.on('click', function (e) {
         return alert('Geolocation not supported by your browser');
     }
 
+    locationButton.attr('disabled', 'disabled').text('Sending location...');
+
     navigator.geolocation.getCurrentPosition(function (position) {
+        locationButton.removeAttr('disabled').text('Send location');
         socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude,
         });
     }, function () {
+        locationButton.removeAttr('disabled').text('Send location');
         alert('Unable to fetch location');
     });
 });
